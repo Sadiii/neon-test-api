48922744a0fd7b4202b1ffa652f8cee02e7f0987
Sadia Zaman <sadiazaman@Sadias-MacBook-Air.local>
2023-04-16 10:42:51 +0500

    third commit

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,58 +1,13 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import postgres from "postgres";
-
-// export async function GET(request: NextRequest) {
-//   const conn = postgres({
-//     ssl: require,
-//   });
-//   const result = await conn.unsafe("SELECT * FROM playing_with_neon");
-//   console.log("backend result", result);
-//   return new NextResponse(JSON.stringify(result));
-// }
-
-
 import { NextRequest, NextResponse } from "next/server";
-
-import { Pool } from 'pg';
-
-import {
-  Kysely,
-  PostgresDialect,
-  Generated,
-  ColumnType,
-  Selectable,
-  Insertable,
-  Updateable,
-  
-} from 'kysely';
-
-
-interface playing_with_neon_Table {
-  id: Generated<number>
-  name: string
-  value: number
-}
-
-interface Database {
-  playing_with_neon: playing_with_neon_Table
-}
-
+import postgres from "postgres";
 
 export async function GET(request: NextRequest) {
-  const db = new Kysely<Database>({
-    dialect: new PostgresDialect({
-      pool: new Pool({
-        ssl: true,
-        connectionString: process.env.NEON_DATABASE_URL!
-      })
-    })
+  const conn = postgres({
+    ssl: require,
   });
-  
-  const result = await db
-  .selectFrom("playing_with_neon")
-  .selectAll()
-  .execute()
-  
+  const result = await conn.unsafe("SELECT * FROM playing_with_neon");
   return new NextResponse(JSON.stringify(result));
 }
+
+
 
--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  env: {
-    // PGHOST: 'ep-hidden-grass-536062.us-east-2.aws.neon.tech',
-    // PGDATABASE: 'neondb',
-    // PGUSER:'Sadiii',
-    // PGPASSWORD:'c2Syd1lUkxZu'
-    NEON_DATABASE_URL: 'postgres://Sadiii:c2Syd1lUkxZu@ep-hidden-grass-536062.us-east-2.aws.neon.tech/neondb',
-  }
 }
 
 module.exports = nextConfig
